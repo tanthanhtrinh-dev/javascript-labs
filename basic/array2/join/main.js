e636b0814e77fda71a274496180827799881878a
Tan Trinh <tanthanhtrinh.dev@gmail.com>
2023-05-31 15:15:22 +0700

    update data

--- basic/array2/join/main.js
+++ basic/array2/join/main.js
@@ -1,22 +1,23 @@
-
 /**
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
  */
 
-const basicJoin = ()=>{
+const basicJoin = () => {
+  const elements = ['Fire', 'Air', 'Water'];
 
-    const elements = ['Fire', 'Air', 'Water'];
+  console.log(elements.join());
+  // expected output: "Fire,Air,Water"
 
-    console.log(elements.join());
-    // expected output: "Fire,Air,Water"
+  console.log(elements.join(''));
+  // expected output: "FireAirWater"
 
-    console.log(elements.join(''));
-    // expected output: "FireAirWater"
+  console.log(elements.join('-'));
+  // expected output: "Fire-Air-Water"
 
-    console.log(elements.join('-'));
-    // expected output: "Fire-Air-Water"
+  console.log(elements.join(' OR '));
+};
 
-    console.log(elements.join(' OR '));
-}
+//basicJoin();
 
-basicJoin();
+const shopifyOrderIds = [5584029614244, 5584079061156];
+console.log(shopifyOrderIds.toString());

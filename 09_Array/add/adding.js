d998d32a452f2dcd56db9f929ab152d60d2df052
Tan.Trinh <tantrinh@tpfgroup.cc>
2024-05-29 13:13:29 +0700

    Working on array in javascript

---
+++ 09_Array/add/adding.js
@@ -0,0 +1,23 @@
+const users = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 17 },
+    { id: 3, name: "Charlie", age: 20 }
+  ];
+
+  const userInit = [];
+  
+  // New user object to be added
+  const newUser = { id: 4, name: "David", age: 22 };
+  
+  // Add new user to the users array
+  //users.push(newUser);
+  
+  //console.log(users);
+userInit.push(newUser);  
+console.log(userInit);
+
+users.map()
+
+userInit.push(users);
+console.log(userInit);
+  

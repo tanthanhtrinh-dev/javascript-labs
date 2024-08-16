67106dbb33843300f8d9c59a9be8cc044f831173
Tan.Trinh <tantrinh@tpfgroup.cc>
2024-08-16 13:22:53 +0700

    Compare difference between ?? and ||

--- basic/1_variables/index.js
+++ basic/1_variables/index.js
@@ -1,4 +1,11 @@
 let a = b = 0.2;
 
-console.info(a +"_"+ b);
+//console.info(a +"_"+ b);
 
+let m = {
+    stock: 0,
+    weight: undefined,
+}
+
+console.log(Number.parseFloat(m?.weight || 0.0))
+console.log(Number.parseFloat(m?.weight ?? 0.0));
--- basic/array2/remove-double-object-array/main.js
+++ basic/array2/remove-double-object-array/main.js
@@ -1,14 +1,17 @@
 const arrayWithDuplicates = [
-    { id: 1, name: "Alice" },
-    { id: 2, name: "Bob" },
-    { id: 1, name: "Alice" },
-    { id: 3, name: "Charlie" },
-    { id: 2, name: "Bob" }
-  ];
-  
-  const uniqueArray = Array.from(
-    new Set(arrayWithDuplicates.map(JSON.stringify))
-  ).map(JSON.parse);
-  
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 1, name: 'Alice' },
+  { id: 3, name: 'Charlie' },
+  { id: 2, name: 'Bob' },
+];
+
+try {
+  const uniqueArray = Array.from(new Set(arrayWithDuplicates.map(JSON.stringify))).map(JSON.parse);
   console.log(uniqueArray);
   console.table(uniqueArray);
+} catch (error) {
+  console.log(error);
+} finally {
+  console.table(arrayWithDuplicates);
+}
